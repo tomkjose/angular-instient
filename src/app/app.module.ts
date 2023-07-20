77bfe339b4b4e8dbadaa71bae06b7e23addb658b
Zypher <44130152+IceAge95@users.noreply.github.com>
2023-07-20 23:43:22 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSectionComponent } from './left-section/left-section.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { RightSectionComponent } from './right-section/right-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSectionComponent,
+    MainSectionComponent,
+    RightSectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/left-section/left-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-section',
+  templateUrl: './left-section.component.html',
+  styleUrls: ['./left-section.component.css']
+})
+export class LeftSectionComponent {
+
+}
